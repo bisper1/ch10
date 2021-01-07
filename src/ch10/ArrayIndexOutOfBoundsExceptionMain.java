a88984f0c4a3b9ee13afe4942c2aa34d236752e3
bisper1 <bisper@naver.com>
2021-01-07 14:28:47 +0900

    ArrayIndexOutOfBoundsException 사용..

--- src/ch10/ArrayIndexOutOfBoundsExceptionMain.java
+++ src/ch10/ArrayIndexOutOfBoundsExceptionMain.java
@@ -3,6 +3,10 @@ package ch10;
 public class ArrayIndexOutOfBoundsExceptionMain {
 
 	public static void main(String[] args) {
+//		String data1 = args[0];
+//		String data2 = args[1];
+//		System.out.println("args[0] : " + data1);
+//		System.out.println("args[1] : " + data2);
 		
 		try {
 			String data1 = args[0];
